90de6aa61ee5c519a701992265769f64adfc4a50
DannielNavas <dannielnavas@gmail.com>
2023-06-19 18:05:32 -0500

    💄 the design of the card is created for the articles

--- src/app/components/shared/post-cards/post-cards.component.ts
+++ src/app/components/shared/post-cards/post-cards.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-cards',
   templateUrl: './post-cards.component.html',
-  styleUrls: ['./post-cards.component.scss']
+  styleUrls: ['./post-cards.component.scss'],
 })
 export class PostCardsComponent {
-
+  @Input() post!: any;
 }
