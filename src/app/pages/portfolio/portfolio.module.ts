eb73f93dd2909dc22a5fc5832cabfe4e2968f04d
DannielNavas <dannielnavas@gmail.com>
2023-06-19 18:06:40 -0500

    💄 the design of the cards created is incorporated

--- src/app/pages/portfolio/portfolio.module.ts
+++ src/app/pages/portfolio/portfolio.module.ts
@@ -2,11 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeaderModule } from 'src/app/components/header/header.module';
+import { CardProjectsModule } from 'src/app/components/shared/card-projects/card-projects.module';
+import { CardStatisticsModule } from 'src/app/components/shared/card-statistics/card-statistics.module';
+import { CardVideoModule } from 'src/app/components/shared/card-video/card-video.module';
+import { PostCardsModule } from 'src/app/components/shared/post-cards/post-cards.module';
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { PortfolioComponent } from './portfolio.component';
 
 @NgModule({
   declarations: [PortfolioComponent],
-  imports: [CommonModule, PortfolioRoutingModule, HeaderModule],
+  imports: [
+    CommonModule,
+    PortfolioRoutingModule,
+    HeaderModule,
+    PostCardsModule,
+    CardProjectsModule,
+    CardVideoModule,
+    CardStatisticsModule,
+  ],
 })
 export class PortfolioModule {}
