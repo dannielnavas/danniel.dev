b6c3c49946769b0bf70eca7f496af3ad6428caf1
DannielNavas <dannielnavas@gmail.com>
2023-06-19 18:04:55 -0500

    💄 the video card design is created

--- src/app/components/shared/card-video/card-video.module.ts
+++ src/app/components/shared/card-video/card-video.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { CardVideoComponent } from './card-video.component';
 
-
-
 @NgModule({
-  declarations: [
-    CardVideoComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CardVideoComponent],
+  imports: [CommonModule],
+  exports: [CardVideoComponent],
 })
-export class CardVideoModule { }
+export class CardVideoModule {}
