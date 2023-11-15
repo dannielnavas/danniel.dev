81d600f71cfc5ec18708023ec2553311787f7f92
Danniel Navas <dannielnavas@gmail.com>
2023-11-15 18:47:27 -0500

    ✨ Update dependencies and fix pipe transform function.

--- src/app/shared/pipes/repo-list.pipe.ts
+++ src/app/shared/pipes/repo-list.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IResponseGithub } from 'src/app/core/models/response-github.interface';
 
 @Pipe({
   name: 'randomlist',
 })
 export class RepoListPipe implements PipeTransform {
-  transform(value: any[], ...args: number[]): any[] {
+  transform(value: IResponseGithub[], ...args: number[]): IResponseGithub[] {
     if (value && Number(args[0]) !== 0) {
       return value.sort(() => 0.5 - Math.random()).slice(0, args[0]);
     }
